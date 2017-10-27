89bea0b4520f5ab7755b6f5ec55b4a1fd21d2806
Mathieu Clement <mclement2@dons.usfca.edu>
2017-10-26 19:38:15 -0700

    Rename

--- scripts/list_nam_files.sh
+++ scripts/list_nam_files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ssh bass01 'bash -s' <<'ENDSSH'
-hdfs dfs -ls /tmp/cs686/nam | awk '{ print $8; }' | grep -v 'nam_mini' | xargs
+hdfs dfs -ls /tmp/cs686/nam | awk '$8 ~ /.+\.tdv$/ { print $8; }' | grep -v 'nam_mini' | xargs
 ENDSSH
