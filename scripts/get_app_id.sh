3545018a2133bbc8d36c002fbfb5f6894ce42c32
Mathieu Clement <mclement2@dons.usfca.edu>
2017-10-26 19:45:38 -0700

    Do not consider jobs with spaces in the name

--- scripts/get_app_id.sh
+++ scripts/get_app_id.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ssh bass01 'bash -s' <<'ENDSSH'
-yarn application -list  | awk '$6 == "mclement2" { print $1; }'
+yarn application -list  | awk '$4 == "mclement2" { print $1; }'
 ENDSSH
