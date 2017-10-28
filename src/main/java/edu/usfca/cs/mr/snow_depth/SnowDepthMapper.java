c1f5415b70edf1d80ddb72a525e72b8dbf0581e9
Mathieu Clement <mclement2@dons.usfca.edu>
2017-10-28 05:27:02 -0700

    Implement snow depth

---
+++ src/main/java/edu/usfca/cs/mr/snow_depth/SnowDepthMapper.java
@@ -0,0 +1,29 @@
+package edu.usfca.cs.mr.snow_depth;
+
+import edu.usfca.cs.mr.util.Observation;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * Mapper: Reads line by line, emit <geohash, snow_depth> pairs
+ */
+public class SnowDepthMapper extends Mapper<LongWritable, Text, Text, FloatWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException {
+
+        Object[] features = Observation.getFeatures(
+                value.toString(),
+                new int[]{2, 51},
+                new Class[]{String.class, Float.class});
+        String geohash = (String) features[0];
+        float snowDepth = (float) features[1]; // in meters
+
+        context.write(new Text(geohash), new FloatWritable(snowDepth));
+    }
+}
---
+++ src/main/java/edu/usfca/cs/mr/snow_depth/SnowDepthReducer.java
@@ -0,0 +1,37 @@
+package edu.usfca.cs.mr.snow_depth;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * Reducer: Input to the reducer is the output from the mapper. It receives
+ * geohash, list<snowDepth> pairs.  Emits <geohash, average> for geohashes
+ * that have snow all year (i.e. for all records seen).
+ */
+public class SnowDepthReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
+
+    @Override
+    protected void reduce(Text key, Iterable<FloatWritable> values, Context context)
+            throws IOException, InterruptedException {
+        float total = 0;
+        int count = 0;
+        boolean hasNonZeroValue = false;
+        // calculate the total count
+        for (FloatWritable val : values) {
+            count++;
+            float f = val.get();
+            total += f;
+            if (Math.abs(f - 0.0001f) < 0.0001f) { // d < 1 mm considered zero
+                hasNonZeroValue = true;
+            }
+        }
+        float average = total / (float) count;
+        if (!hasNonZeroValue && average > 0f) {
+            context.write(key, new FloatWritable(average));
+        }
+    }
+
+}
